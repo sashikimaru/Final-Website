02ff1d34ad2f7d5b6f7ecd6ad56fd80088fd2a78
sashikimaru <48923056+sashikimaru@users.noreply.github.com>
2019-04-07 04:28:28 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,39 @@
+
+var value = 100;
 var x = 0;
 var y = 0;
-var value = 100;
-
 function setup() {
-  var canvas = createCanvas(windowWidth, 400);
+  var canvas = createCanvas(windowWidth, 500);
   canvas.parent('header');
 }
 
 function draw() {
-  //over
-  background(21,1,77);
-  
-  
-  
-  if (x < 380){
+
+	background (21,1,77);
+    if (x < 380){
 y = pow(x,.5)+36;
-  x = x+1;}
+  x = x+.3;}
 
   else if(x=380){value = 0;
   }
-  
-  fill(65,value+71,0);
-  circle(x , y, 25);
-  
-  noStroke();
-  fill(21,1,77);
-  rect(0, 340 , 500, 340);
-  
-  fill(value + 73,0,159);
-  rect(25,0,55,340);
-  
-  fill(value+16,0,125);
-  triangle(80,0,80,70,160,0);
-  triangle(80,40,80,340,500,340);
-  
-  fill(value+73,0,159);
+    
+    fill(255,255, value + 100);
+    ellipse(x,y,50,50);
+    
+    fill(21,1,77);
+    rect(0,340,windowWidth,340);
+    
+    fill(value + 73,0,159);
+    rect(25,0,55,340);
+    
+    fill(value+16,0,125);
+    triangle(80,0,80,70,160,0);
+    triangle(80,40,80,340,500,340);
+    
+    fill(value+73,0,159);
+    triangle(80,40,80,70,100,53);
+    
+    fill(value+73,0,159);
   triangle(80,40,80,70,100,53);
   
   fill(value+16,0,125);
@@ -59,29 +58,23 @@ y = pow(x,.5)+36;
   rect(280,160,80, 180);
   rect(360, 80, 80, 260);
   rect(360, 0,80, 30);
-  
- 
     
-  fill(value+16,0,125);
-  triangle(440,80,440,340,800,340);
-  triangle(440,0,440,30,480,0);
+    fill(value+16,0,125);
+  rect(440,80,windowWidth,260);
+  rect(440,0,windowWidth,30);
   
   fill(0,value+58,216);
   rect(25,40, 55, 30);
-
-  
-  
-  
-  //under
-  
-  fill(0,value+25,113);
+    
+    fill(0,value+25,113);
   triangle(80,340,80,640,425,340);
   triangle(140,350,140.5,440,280,340);
-  
-  fill(18,value+61,0);
+    
+    
+    fill(18,value+61,0);
   rect(80,340,80,60);
-  
-  fill(0,value+58,216);
+    
+   fill(0,value+58,216);
   rect(120,400,20,40);
   
   fill(4,199,value+65);
@@ -98,15 +91,17 @@ y = pow(x,.5)+36;
   rect(360,340,80,160);
   
   fill(0,value+25,113);
-  rect(440,340,60,160);
-
-  
+  rect(440,340,windowWidth,160);
+ 
   fill(55,value+6,63);
-  rect(25,340, 55,160);
-  
-  textSize(30);
-  fill(255,255,255);
-  text("Nam Nguyen",50, 200);
-  fill(250);
-  text("Nam Nguyen",51,201);
-}  
+  rect(25,340, 55,160);  
+
+	textSize(30);
+	fill(255);
+	text("Nam",50,200); //shadow
+	text("Nguyen",50,225); //shadow
+	fill(250);
+	text("Nam",49,199);
+	text("Nguyen",49,224);
+
+}
